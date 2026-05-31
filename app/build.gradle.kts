269789780c2b780ca8414edc62e7ebafa6c121b7
shijuhao <19136769323@163.com>
2026-05-31 17:10:11 +0800

    fix bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.material)
 
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
