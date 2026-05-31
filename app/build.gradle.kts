10d7ad1eafb4810d84e2a856340ce1468da609de
shijuhao <19136769323@163.com>
2026-05-31 16:13:42 +0800

    fix error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material)
 
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
